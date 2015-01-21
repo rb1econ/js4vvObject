4d6ba294e19b0dbdd18f05724a8c1fb74ea2ae3b
Ryan budny <rb1econ@gmail.com>
2015-01-21 11:11:15 -0700

    from js4vvBonus1 directory

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 var x=true;
 
 
-var y=true
+var y=true;
 
 
-var victimsArrObj = []
+var victimsArrObj = [];
+var volunteerArrObj = [];
 
 while (x) {
 	var victims = prompt("how many victims would you like to enter?");
-	if (isNaN(victims)===true)  { 
+	if (isNaN(victims))  { 
 		alert("please enter a number");
 	}
 	else {
 		x=false;
-		victimsArrObj.length = victims;
-		
+
 		for (var i=victims; i>0; i--) {
 			var n = prompt("thanks, please enter the name of one of the victims.");
 			var p = prompt("please enter this victim's phone number");
@@ -32,41 +32,43 @@ console.log(victimsArrObj);
 
 	 
 
-// while (y) {
-// 	var volunteers = prompt("how many volunteers would you like to enter?");
-// 	if (isNaN(victims)===true) {
-// 		alert("please enter a number");
-// 	}
-// 	else {
-// 		y=false;
+while (y) {
+	var volunteers = prompt("how many volunteers would you like to enter?");
+	if (isNaN(volunteers)) {
+		alert("please enter a number");
+	}
+	else {
+		y=false;
 
-// 		for (var i=victims; i>0; i--) {
-// 			var name = prompt("thanks, please enter the name of one of the volunteers.");
-// 				nameArray2.push(name);
-// 			var phone = prompt("please enter this volunteer's phone number");
-// 				phoneArray2.push(phone);
-// 			var street = prompt("please enter this volunteer's street name.");
-// 				streetArray2.push(street);
-// 		}
+		for (var i=volunteers; i>0; i--) {
+			var a = prompt("thanks, please enter the name of one of the volunteers.");
+			var b = prompt("please enter this volunteer's phone number");
+			var c = prompt("please enter this volunteer's street name.");
 
-// 	}
-// }
+			var set2 = {name: a, phone: b, street: c};
 
+			volunteerArrObj.push(set2)
+		}
 
-
+	}
+}
 
 
 
-// var finalAlertString = "The total number of victim(s) is " + victims + ". ";
+var finalAlertString = "The total number of victim(s) is " + victimsArrObj.length + ". ";
+console.log(finalAlertString);
+for (var i=victims-1; i>=0; i--) {
+		var victimInfo = "Victim " + victimsArrObj[i]['name'] + "'s phone number is " + victimsArrObj[i].phone + " and he/she lives on " + victimsArrObj[i].street + " street. ";
+		finalAlertString += victimInfo;
+};
+console.log(finalAlertString);
 
-// for (var i=victims-1; i>=0; i--) {
-// 		var victimInfo = "Victim " + nameArray[i] + "'s phone number is " + phoneArray[i] + " and he/she lives on " + streetArray[i] + " street. ";
-// 		finalAlertString += victimInfo;
-// };
+for (var i=volunteers-1; i>=0; i--) {
+		console.log(volunteerArrObj[i])
+		var volunteerInfo = "Volunteer" + volunteerArrObj[i].name + "'s phone number is " + volunteerArrObj[i].phone + " and he/she lives on " + volunteerArrObj[i].street + " street. ";
+		finalAlertString += volunteerInfo;
+};
+console.log(finalAlertString);
 
-// for (var i=victims-1; i>=0; i--) {
-// 		var volunteerInfo = "Volunteer" + nameArray[i] + "'s phone number is " + phoneArray2[i] + " and he/she lives on " + streetArray2[i] + " street. ";
-// 		finalAlertString += volunteerInfo;
-// };
+alert(finalAlertString);
 
-// alert(finalAlertString);
